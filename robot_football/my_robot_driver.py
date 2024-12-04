6b893225f06e3b4861083008c5a4f7952b3a3c0b
McGechie <u33km21@abdn.ac.uk>
2024-12-04 06:30:12 +0000

    Got the goal detection working

--- robot_football/my_robot_driver.py
+++ robot_football/my_robot_driver.py
@@ -2,6 +2,7 @@ import rclpy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float32MultiArray
 from controller import Supervisor
+import random
 
 HALF_DISTANCE_BETWEEN_WHEELS = 0.045
 WHEEL_RADIUS = 0.025
@@ -29,8 +30,19 @@ class MyRobotDriver:
         self.__node = rclpy.create_node("my_robot_main")
         self.__node.create_subscription(Twist, "cmd_vel", self.__cmd_vel_callback, 1)
         self.ballPublisher = self.__node.create_publisher(Float32MultiArray, "/ball_pos",  qos_profile=1)
-        # create the ball using the ROS2 Supervisor
+        
+        #create the ball and store some of its details as an attribute of this class instance
+        #ideally we would have a ball class that stores some of this but idc
         self.createBall()
+        self.ball_translation_field = self.ballNode.getField('translation')
+
+        #counts the number of goals, currently we dont do anything with this
+        self.goals = 0
+
+        #stores a reference to the physical robot as an attribute of this class instance
+        self.puck = self.supervisor.getFromDef("PUCK")
+        #similarly to the ball, we get a reference to the position of the robot so we can change it later
+        self.puck_translation_field = self.puck.getField('translation')
 
     def createBall(self):
         """Creates a new ball when the ball goes out of bounds."""
@@ -78,3 +90,14 @@ class MyRobotDriver:
 
         self.__left_motor.setVelocity(command_motor_left)
         self.__right_motor.setVelocity(command_motor_right)
+
+
+        #this handles goal logic, detecting a goal has happened and randomizing the positions of the ball and the robot
+        #currently does not clear their velocity but this is a trivial modification
+        if self.ballNode.getPosition()[0] > 1.75:
+            new_value = [random.uniform(-1.35,1.35), random.uniform(-0.7,0.7), 0.1]
+            self.ball_translation_field.setSFVec3f(new_value)
+
+            newer_value = [random.uniform(-1.35,1.35), random.uniform(-0.7,0.7), 0.001]
+            self.puck_translation_field.setSFVec3f(newer_value)
+            self.goals += 1
